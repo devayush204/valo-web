df6a7fb536528bcb04250200c07e40b2c4a88fe1
ayush <ayushbhatt@204gmail.com>
2023-08-10 21:16:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+const App = () => {
+  
+
+  return (
+    <>
+     <Navbar/>
+     <Home/>
+    </>
+  )
+}
+
+export default App;
