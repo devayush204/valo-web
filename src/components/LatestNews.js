f49b8992c2cf24ad010e121b112024cd4b2b13ac
Ayush bhatt <124452247+devayush204@users.noreply.github.com>
2023-09-13 21:06:34 +0530

    Update LatestNews.js

--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -5,11 +5,11 @@ const LatestNews = () => {
         <section className="bg-[#ECE8E1] z-10 py-16 relative pb-[120px] overflow-hidden">
             <p className='text-[260px] absolute valo-text z-[10] -bottom-[100px] '><pre>WE ARE</pre> <pre className=''>    VALORANT</pre> </p>
             <div className="container mx-auto relative flex justify-center items-center z-[20]">
-                <span className=" bottom-[270px] z-10 left-4 text-[90px] text-[#ED4252] tracking-tighter  absolute text">LATEST NEWS</span>
+                <span className=" bottom-[270px] z-10 left-4 text-[90px] text-[#ED4252] tracking-tighter  absolute text uppercase">latest news</span>
                 <span className='absolute -top-6  right-10'>
                     <a href="/" className='group'>
                     <div className='flex gap-3 '>
-                    <p className='text-[#ED4252] text-sm font-semibold font-tungsten'>GO TO NEWS PAGE </p>
+                    <p className='text-[#ED4252] text-sm font-semibold font-tungsten uppercase'>go to news page </p>
                     <svg className='w-[30px] transition-transform group-hover:translate-x-3 duration-500' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 15"><path d="M16.2.5 19.9 4m4.6 3.5-8.3 7" fill="none" stroke="#ff4655"></path><path fill="none" stroke="#ff4655" stroke-miterlimit="10" d="M16 7.5H0"></path></svg>
                     </div>
                     </a>
@@ -20,7 +20,7 @@ const LatestNews = () => {
                         <img src={"/news1.webp"} alt="News 1" className="group overflow-hidden w-full h-[250px] object-cover" />
                         <div className="flex gap-12 mt-3 items-center mb-4 ">
                             <h3 className="text-xs text-zinc-500 font-semibold ">12/12/23</h3>
-                            <p className="text-sm text-red-600 font-semibold">GAME UPDATES</p>
+                            <p className="text-sm text-red-600 font-semibold uppercase">game updates</p>
                         </div>
                         <a href="#" className="text-zinc-600 text-[19px]  font-bold tracking-wider ">VALORANT PATCH NOTES</a>
                         <div className=" absolute -left-[500px]  top-0 w-[100%] h-[77%] bg-[#fd3a4a] opacity-0 -skew-x-12 transf group-hover:opacity-50 group-hover:left-[10px] transition-all duration-300 ease-in-out"></div>
